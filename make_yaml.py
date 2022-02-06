0a65f0e930a878004c071e09eec62973c005f563
astra <zorkin.roman@gmail.com>
2022-02-06 17:40:20 +0300

    Updates scheme in make_yaml file

--- make_yaml.py
+++ make_yaml.py
@@ -1,19 +1,35 @@
 from pathlib import Path
 
+import yaml
 
 tables_scheme = {
     'file_path': Path('configuration/tables.yaml'),
     'tables': {
         'speakers': {
             'columns': {
-                'name': None,
+                'id': {
+                    'format': ['int', 'NOT NULL'],
+                    'primary_key': [True, 'AUTO_INCREMENT'],
+                    'description': '',
+                },
+                'name': {
+                    'format': ['text', 'NOT NULL'],
+                    'primary_key': [False],
+                    'description': '',
+                },
                 'surname': None,
-                'telegram': None,
-                'facebook': None,
-                'vk': None,
+                'organization': None,
+                'experience': None,
+                'activity_sphere ': None,
+                'other_information': None,
             },
         },
         'meetups': None,
+        'social_net': {
+            'name': None,
+            'speaker': None,
+            'address': None,
+        },
         'messages': {
             'columns': {
                 'general_theme': None,
@@ -25,9 +41,13 @@ tables_scheme = {
         },
         'themes': None,
         'areas': None,
-    }
+    },
 }
 
 
 def create_file(file_scheme) -> None:
-    pass
+    with open(file_scheme['file_path'], 'w') as target_file:
+        yaml.dump(file_scheme['tables'], target_file, default_flow_style=False)
+
+
+create_file(tables_scheme)
